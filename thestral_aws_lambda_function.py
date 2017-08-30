e86c9057f46a3981bf69677c8b0e9b0fc5f2faef
Vinod Sharma <vinodsharma.mimit@gmail.com>
2017-08-30 12:53:07 -0700

    renaming variables

--- thestral_aws_lambda_function.py
+++ thestral_aws_lambda_function.py
@@ -1,4 +1,16 @@
 import boto3
+import logging
+import sys
+
+logger = logging.getLogger('thestral_aws_lambda')
+logger.setLevel(logging.DEBUG)
+
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
 
 
 def submit_job(batch_client, job_definition_name, job_name, job_queue_name):
@@ -7,16 +19,14 @@ def submit_job(batch_client, job_definition_name, job_name, job_queue_name):
         jobName=job_name,
         jobQueue=job_queue_name,
     )
-
-    print(response)
-    print "Checking New Version"
+    logger.info("Submit job response %s", response)
 
 
 def lambda_handler(event, context):
-    print("Starting")
+    logger.info("submit_job Started")
     batch_client = boto3.client('batch')
-    job_queue_name = 'V10_M4OnDemandQueue'
-    job_definition_name = 'V10_M4OnDemandJobDefinition'
-    job_name = 'V10_M4OnDemandJob'
+    job_queue_name = 'thestral_job_queue'
+    job_definition_name = 'thestral_job_definition'
+    job_name = 'thestral_job'
     submit_job(batch_client, job_definition_name, job_name, job_queue_name)
-    print("Ending")
+    logger.info("submit_job Completed")
